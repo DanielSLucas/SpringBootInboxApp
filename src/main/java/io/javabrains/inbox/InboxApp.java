9ea32b26aeedafe918c7b80d28002ab6d2a82c6a
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2024-04-19 17:09:35 -0300

    feat: show folders list

--- src/main/java/io/javabrains/inbox/InboxApp.java
+++ src/main/java/io/javabrains/inbox/InboxApp.java
@@ -2,16 +2,25 @@ package io.javabrains.inbox;
 
 import java.nio.file.Path;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.javabrains.inbox.folders.Folder;
+import io.javabrains.inbox.folders.FolderRepository;
+
 
 @SpringBootApplication
 @RestController
 public class InboxApp {
+
+	@Autowired
+	FolderRepository folderRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(InboxApp.class, args);
@@ -21,6 +30,14 @@ public class InboxApp {
 	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
 		Path bundle = astraProperties.getSecureConnectBundle().toPath();
 		return builder -> builder.withCloudSecureConnectBundle(bundle);
+	}
+
+	@PostConstruct
+	public void init() {
+		
+		folderRepository.save(new Folder("DanielSLucas", "Inbox", "blue"));
+		folderRepository.save(new Folder("DanielSLucas", "Sent", "green"));
+		folderRepository.save(new Folder("DanielSLucas", "Important", "yellow"));
 	}
 
 }
---
+++ src/main/java/io/javabrains/inbox/folders/Folder.java
@@ -0,0 +1,27 @@
+package io.javabrains.inbox.folders;
+
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.core.mapping.CassandraType;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
+import org.springframework.data.cassandra.core.mapping.Table;
+import org.springframework.data.cassandra.core.mapping.CassandraType.Name;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(value = "folders_by_user")
+public class Folder {
+  
+  @PrimaryKeyColumn(name = "name_id", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
+  private String id;
+
+  @PrimaryKeyColumn(name = "label", ordinal = 1, type = PrimaryKeyType.CLUSTERED)
+  private String label;
+
+  @CassandraType(type = Name.TEXT)
+  private String color;
+}
