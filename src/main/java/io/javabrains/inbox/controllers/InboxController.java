5e63d2df68a9f4fe0ef1a1a1acd12bd0b43c5dd3
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2024-04-30 16:40:19 -0300

    feat: folder list layout

--- src/main/java/io/javabrains/inbox/controllers/InboxController.java
+++ src/main/java/io/javabrains/inbox/controllers/InboxController.java
@@ -12,12 +12,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import io.javabrains.inbox.folders.Folder;
 import io.javabrains.inbox.folders.FolderRepository;
+import io.javabrains.inbox.folders.FolderService;
 
 @Controller
 public class InboxController {
   
   @Autowired
 	private FolderRepository folderRepository;
+
+  @Autowired
+	private FolderService folderService;
 
   @GetMapping(value = "/")
   public String homePage(
@@ -29,7 +33,11 @@ public class InboxController {
     } 
     
     String userId = principal.getAttribute("login");
+    
+    List<Folder> defaultFolders = folderService.fetchDefaultFolders(userId);
     List<Folder> userFolders = folderRepository.findAllById(userId);
+
+    model.addAttribute("defaultFolders", defaultFolders);
     model.addAttribute("userFolders", userFolders);
 
     return "inbox";
